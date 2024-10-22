f1f664031b5a58d9629054f8f544a8a9e24be6b5
Box_yld <103340124+boxyld@users.noreply.github.com>
2024-10-22 19:11:21 +0300

    Update Pizza.py

--- Pizza.py
+++ Pizza.py
@@ -1,2 +1,10 @@
-x = "It's Pizza Time!"
+
+#python lib
+import webbrowser
+
+#overenginnerd way to print stupid text
+x = "It's shawerma Time!"
 print(x)
+
+#your webbroswer will open this link
+webbrowser.open("https://www.youtube.com/watch?v=l8lQoRZu64I&t=65s")
